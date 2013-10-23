4d8c6f424e645dd2ab40ee40e21ead137612bfd7
John Lato <jwlato@tsurucapital.com>
2013-10-22 21:33:47 -0500

    Rotations.hs: add squares' and notes

--- examples/Rotations.hs
+++ examples/Rotations.hs
@@ -11,7 +11,15 @@ import           Diagrams.Prelude
 
 n = 20
 
+-- Ideally, this function would perform well:
 squares = iterateN (n*n) (rotateBy (1/(n*n))) (square 1)
+
+-- but it doesn't, due to the iterated calls to rotateBy.
+-- this alternative has much better performance:
+squares' = [square 1 # rotateBy (fromIntegral x/fromIntegral (n*n))
+            | x <- [0..n*n::Int]]
+-- in order to get good performance out of nested transformations, it's
+-- probably necessary to make significant changes to the Transform type.
 
 main = defaultMain benchDiagram
 
